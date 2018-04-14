66f1271a2f8ed7099499f77ff9fb0e7745fe341d
Stephane Candelas <stephanecandelas@gmail.com>
2018-04-14 18:43:52 +0100

    update index.html & script.js

--- script.js
+++ script.js
@@ -42,11 +42,19 @@ $(document).ready(function(){
     }
   });
 
-  var map = new GMaps({
-    div: '.map',
-    lat: 51.5098942, /* 51.5098942,-0.1486348 */
-    lng: -0.1486348
-});
+//   var map = new GMaps({
+//     div: '.map',
+//     lat: 51.5098942, /* 51.5098942,-0.1486348 */
+//     lng: -0.1486348
+// });
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
 // map.addMarker({
 // lat: 51.5098942,
